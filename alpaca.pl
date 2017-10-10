0c9f049788ff7a6e4eda8c84e0c0d006d32a9944
Kim Chen <kchen@stetson.edu>
2017-10-10 11:46:34 -0400

    Changed vuln/4 definition -- added rolename, tag; Changed achieve_goal/5 definition -- added UsedTags

--- alpaca.pl
+++ alpaca.pl
@@ -1,16 +1,22 @@
+% vuln( [prereqs], [result], rolename, tag )
+vuln([web_access], [database_queries], sql, initial_web).
+vuln([database_queries], [hashed_passwords, user_list], no_effect, sql_query).
+vuln([hashed_passwords], [passwords], no_effect, password_cracking).
+vuln([passwords, user_list, ssh_server], [shell_access], no_effect, shell_access).
 
-vuln([web_access], "SQL Injection", [database_queries]).
-vuln([database_queries], "SQL Query", [hashed_passwords, user_list]).
-vuln([hashed_passwords], "Password Cracking", [passwords]).
-vuln([passwords, user_list, ssh_server], "Shell Access", [shell_access]).
+vuln([web_access], [remote_file_inclusion], no_effect, initial_web).
+vuln([remote_file_inclusion], [shell_access], no_effect, shell_access).
 
-achieve_goal(Goal, InitialState, _, _) :- member(Goal, InitialState).
-achieve_goal(Goal, InitialState, Attempted, [(Input, Description, Output)|Vulns]) :-
-    vuln(Input, Description, Output),
-    \+member((Input, Description, Output), Attempted),
-    intersection(Input, InitialState, Input),
-    union(Output, InitialState, NewState),
-    achieve_goal(Goal, NewState, [(Input, Description, Output)|Attempted], Vulns).
+vuln([login_page], [database_queries], sql, intial_web).
 
-% example: achieve_goal(shell_access, [web_access, ssh_server], [], Vulns).
+% achieve_goal( Goal, InitialState, [Attempted], [UsedTags], [Vulns] )
+achieve_goal(Goal, InitialState, _, _, _) :- member(Goal, InitialState).
+achieve_goal(Goal, InitialState, Attempted, UsedTags, [(Input, Output, File, Tag)|Vulns]) :-
+		vuln(Input, Output, File, Tag),
+		\+member((Input, Output, File, Tag), Attempted),
+		\+member(Tag, UsedTags),
+		intersection(Input, InitialState, Input),
+		union(Output, InitialState, NewState),
+		achieve_goal(Goal, NewState, [(Input, Output, File, Tag)|Attempted], [Tag|UsedTags], Vulns).
 
+% example: achieve_goal(shell_access, [web_access, ssh_server], [], [], Vulns).
