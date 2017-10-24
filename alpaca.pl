c79ed70954f162d6e7b2f4b2e02403409090b0dd
Kim Chen <kchen@stetson.edu>
2017-10-24 18:13:35 -0400

    Implementation works

--- alpaca.pl
+++ alpaca.pl
@@ -1,33 +1,71 @@
-% vuln( [prereqs], [result], rolename, tag )
-vuln([web_access], [database_queries], sql, initial_web).
-vuln([database_queries], [hashed_passwords, user_list], no_effect, sql_query).
-vuln([hashed_passwords], [passwords], no_effect, password_cracking).
-vuln([passwords, user_list, ssh_server], [shell_access], no_effect, shell_access).
+%vuln( [prereqs], [result], 
+%					[Role-[key-(pred,[val]),...,key-(pred,[val])]]
 
-vuln([web_access], [remote_file_inclusion], no_effect, initial_web).
-vuln([remote_file_inclusion], [shell_access], no_effect, shell_access).
+vuln('sql injection', [web_access], [database_queries], 
+		 [apache-[version-(only, [2.4])], mysql-[tables-(exists, [users]), version-(only, [5.3])], php-[version-(only, [5.5]), script-(exists, [web])]]).
+vuln('db-query-users', [database_queries], [user_list, hashed_passwords], [mysql-[tables-(exists, [users])]]).
+vuln('crack passwords', [hashed_passwords], [passwords], []).
+vuln('login ssh', [passwords, user_list, ssh_server], [shell_access], []).
 
-vuln([login_page], [database_queries], sql, intial_web).
 
-% achieve_goal( Goal, InitialState, [Attempted], [UsedTags], [Vulns] )
-% 	vulns gets subtracted from when the recursive case is called, so the base case would be an empty list, and the it would build on its way back up
-achieve_goal(Goal, InitialState, _, _, []) :- member(Goal, InitialState).
-achieve_goal(Goal, InitialState, Attempted, UsedTags, [(Input, Output, File, Tag)|Vulns]) :-
-		vuln(Input, Output, File, Tag),
-		\+member((Input, Output, File, Tag), Attempted),
-		\+member(Tag, UsedTags),
+%setof((Configs, Vulns), achieve_goal(shell_access, [web_access, ssh_server], [], [], Configs, Vulns), Result), length(Result, L), print(Result).
+
+% DON'T CHANGE CODE BELOW HERE. YOU WILL BREAK IT.
+
+%achieve_goal( Goal, InitialState, [Attempted], [Vulns] )
+achieve_goal(Goal, InitialState, _, [], [], []) :- member(Goal, InitialState).
+achieve_goal(Goal, InitialState, Attempted, StartingConfigs, AcceptedConfigs, [Description|Vulns]) :-
+		vuln(Description, Input, Output, Configs),
+		\+member((Input, Output, Configs), Attempted),
 		intersection(Input, InitialState, Input),
 		union(Output, InitialState, NewState),
-		achieve_goal(Goal, NewState, [(Input, Output, File, Tag)|Attempted], [Tag|UsedTags], Vulns).
+		achieve_goal(Goal, NewState, [(Input, Output, Configs)|Attempted], StartingConfigs, NewConfigs, Vulns),
+		check_configs(NewConfigs, Configs, AcceptedConfigs).
 
-print_rolenames([]).
-print_rolenames([(_, _, Role, _)|Vulns]) :-
-		nl, print(Role), print_rolenames(Vulns).
+%check_configs(AcceptedConfigs, PendingConfigs, NewConfigs)
+check_configs([], PendingConfigs, PendingConfigs).
+check_configs(AcceptedConfigs, [], AcceptedConfigs).
+check_configs(AcceptedConfigs, PendingConfigs, SortedConfigs) :-
+		select(K-PendingVals, PendingConfigs, RestPendingConfigs),
+		\+member(K-_, AcceptedConfigs),
+		check_configs(AcceptedConfigs, RestPendingConfigs, TmpConfigs),
+		NewConfigs = [K-PendingVals|TmpConfigs],
+		sort(NewConfigs, SortedConfigs).
+check_configs(AcceptedConfigs, PendingConfigs, SortedConfigs) :-
+		select(K-PendingVals, PendingConfigs, RestPendingConfigs),
+		select(K-AcceptedVals, AcceptedConfigs, RestAcceptedConfigs),
+		check_configs(RestAcceptedConfigs, RestPendingConfigs, TmpConfigs),
+		mergeConfigs(AcceptedVals, PendingVals, MergedConfigs),
+		NewConfigs = [K-MergedConfigs|TmpConfigs],
+		sort(NewConfigs, SortedConfigs).
 
-write_to_file(Goal, InitialState, Attempted, UsedTags, Vulns) :-
-		open('steps.txt', write, Stream),
-		forall(achieve_goal(Goal, InitialState, Attempted, UsedTags, Vulns), writeln(Stream, Vulns)),
-		close(Stream).
+only(PriorVals, _, ThisVals, _, _, only) :-
+    union(PriorVals, ThisVals, AllVals),
+    length(AllVals, L),
+    L =< 1.
 
+exists(_, exists, _, _, _, exists).
+exists(PriorVals, only, ThisVals, _, _, only) :-
+    union(PriorVals, ThisVals, PriorVals).
 
-% example: achieve_goal(shell_access, [web_access, ssh_server], [], [], Vulns).
+mergeConfig(Key, (Pred, PriorVals), (ThisPred, ThisVals), Config, Result) :-
+    Check =.. [Pred, PriorVals, ThisPred, ThisVals, Key, Config, NewPred],
+    call(Check),
+    union(PriorVals, ThisVals, AllVals),
+    Result = (NewPred, AllVals).
+
+mergeConfigs([], ThisConfig, ThisConfig).
+mergeConfigs(PriorConfig, [], PriorConfig).
+mergeConfigs(PriorConfig, ThisConfig, SortedConfig) :-
+    select(K-ThisVals, ThisConfig, RestThisConfig),
+    \+member(K-_, PriorConfig),
+    mergeConfigs(PriorConfig, RestThisConfig, TmpConfig),
+    NewConfig = [K-ThisVals|TmpConfig],
+		sort(NewConfig, SortedConfig).
+mergeConfigs(PriorConfig, ThisConfig, SortedConfig) :-
+    select(K-ThisVals, ThisConfig, RestThisConfig),
+    select(K-PriorVals, PriorConfig, RestPriorConfig),
+    mergeConfigs(RestPriorConfig, RestThisConfig, TmpConfig),
+    mergeConfig(K, PriorVals, ThisVals, PriorConfig, NewVals),
+    NewConfig = [K-NewVals|TmpConfig],
+		sort(NewConfig, SortedConfig).
