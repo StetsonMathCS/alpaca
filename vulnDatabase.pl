a3d3a42274d13c5c3257ce2a3ca9b1ae31dc5c31
Kim Chen <kchen@stetson.edu>
2018-02-23 11:06:38 -0500

    Adding to database

--- vulnDatabase.pl
+++ vulnDatabase.pl
@@ -1,5 +1,29 @@
 %vuln( Desciprtion, [prereqs], [result], [Role-[key-(pred, [val]),...,key-(pred,[val])]] )
 
+vuln('host-discovery', [], [discover_hosts], []).
+vuln('port-scanning', [discover_hosts], [open_ports], []).
+
+vuln('open-ftp', [open_ports], [port_21], []).
+vuln('brute-force-login', [port_21], [ftp_access], []).
+vuln('etc-shadow', [ftp_access], [shadow_file], []).
+vuln('crack-passwords', [shadow_file], [passwords], []).
+vuln('ssh-login', [port_22], [server_access])
+
+vuln('open-ssh', [open_ports], [port_22], []).
+vuln('brute-force-login', [port_22], [server_access], []).
+
+vuln('web-access', [open_ports], [web_access], []).
+vuln('enumerate-server', [port_80], [], []).
+
+vuln('directory-traversal', [web_access], [], []).
+
+vuln('sql-injection', [web_access], [database_queries], []).
+vuln('db-query-users', [database_queries], [user_list, hashed_passwords], []).
+vuln('crack-passwords', [hashed_passwords], [passwords], []).
+vuln('login-ssh', [passwords, user_list, ssh_server], [shell_access], []).
+
+
+
 % [apache-[version-(only, [2.4.27-2ubuntu3])], mysql-[tables-(exists, [users]), version-(only, [5.7.20-0ubuntu0.17.10.1])], php-[version-(only,
 % [1:7.1+54ubuntu1]), script-(e    xists, [web])]]).
 
@@ -28,3 +52,9 @@ vuln('discover', [discover], [discover_jboss_server], [server-[application-(only
 vuln('discover-jboss', [discover_jboss_server], [java_object_deserialization], [server-[application-(only, [jboss])]]).
 
 vuln('java-object-deserialization', [java_object_deserialization], [remote_code_execution], []).
+
+
+
+vuln('open-telnet', [open_ports], [port_23], []).
+vuln('server_access', [port_23], [server_access], []).
+vuln('RCE', [server_access], [], []).
