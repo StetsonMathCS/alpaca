ac2c1cf54ff2dc82d0705998a871ddf9e7b9a8fb
Kim Chen <kchen@stetson.edu>
2018-04-15 12:54:44 -0400

    Update database (added configs)

--- vulnDatabase.pl
+++ vulnDatabase.pl
@@ -4,38 +4,58 @@
 vuln('host-discovery', [], [discover_host], []).
 vuln('port-scanning', [discover_host], [open_ports], []).
 
+% FTP
 vuln('open-ftp', [open_ports], [ftp_server], [ftp-[]]).
-vuln('login-root(brute-force)', [ftp_server], [server_access_root], []).
+vuln('login-root(brute-force)', [ftp_server], [server_access_root], 
+        [ftp-[root_password-(only, ['password'])]]).
 
-vuln('open-ssh', [open_ports], [ssh_server], []).
+% SSH
+vuln('open-ssh', [open_ports], [ssh_server], 
+        [role-[value-(only, [ssh])]]).
+
 vuln('login-root(brute-force)', [ssh_server], [server_access_root], 
-        [ssh-[users-(exists, [root])]]).
+        [ssh-[root_password-(only, ['secret'])]]).
+
+vuln('login-root(credentials)', [passwords, ssh_server], [server_access_root], 
+        [ssh-[root_password-(only, ['secret'])]]).
+
 vuln('login-user(brute-force)', [ssh_server], [server_access_user], 
-        [ssh-[users-(exists, [user])]]).
+        [ssh-[users-(exists, [example]),
+              passwords-(exists, ['password'])]]).
 
-vuln('database_queries', [server_access_user], [user_list, hashed_passwords], []).
+vuln('database_queries', [server_access_user], [user_list, hashed_passwords], 
+        [mysql-[users-(exists, [example]),
+                passwords-(exists, ['password'])]]).
 vuln('crack-hashes', [hashed_passwords], [passwords], []).
-vuln('login-root(credentials)', [passwords, ssh_server], [server_access_root], []).
+vuln('login-root(credentials)', [passwords, ssh_server], [server_access_root], 
+        [ssh-[root_password-(only, ['secret'])]]).
 
-vuln('open-web', [open_ports], [port_80], []).
+% Web
+vuln('open-web', [open_ports], [port_80], 
+        [role-[value-(only, [web])]]).
+vuln('web-access', [port_80], [web_access], 
+        [apache-[]]).
 
-vuln('web-access', [port_80], [web_access], []).
-
-vuln('remote_code_execution', [web_admin_access], [remote_code_execution], []).
-vuln('directory-traversal', [web_access], [remote_code_execution], []).
-
-vuln('directory-traversal', [web_access], [bypass_auth], []).
-vuln('bypass-authentication(admin)', [bypass_auth], [web_admin_access], []).
 vuln('login-admin(brute-force)', [web_access, login_page], [web_admin_access], []).
 
-vuln('sql-injection', [web_access], [database_queries], []).
-vuln('db-query-users', [database_queries], [user_list, hashed_passwords], []).
-vuln('crack-hashes', [hashed_passwords], [passwords], []).
 vuln('login-admin(credentials)', [web_access, login_page], [web_admin_access], []).
 
-vuln('directory-traversal', [web_access], [remote_code_execution], []).
-vuln('directory-traversal', [web_access], [bypass_auth], []).
-vuln('directory-traversal', [web_access], [session_hijack], []).
+vuln('sql-injection', [web_access], [database_queries], 
+        [php-[git_repo-(exists, ['https://KimAChen@bitbucket.org/KimAChen/alpaca_sqli.git']),
+              repo_folder-(exists, ['alpaca_sqli']),
+              mysql_db-(exists, ['alpaca_sqli']),
+              mysql_file-(exists, ['users.sql'])]]).
+vuln('db-query-users', [database_queries], [user_list, hashed_passwords], 
+        [mysql-[user-(exists, ['alpaca_sqli']), 
+                password-(exists, ['password'])]]).
+vuln('crack-hashes', [hashed_passwords], [passwords], []).
+vuln('login-root(credentials)', [passwords, ssh_server], [server_access_root], 
+        [ssh-[root_password-(only, ['secret'])]]).
 
+vuln('directory-traversal', [web_access], [bypass_auth], []).
 vuln('bypass-authentication(admin)', [bypass_auth], [web_admin_access], []).
-vuln('remote_code_execution', [web_admin_access], [remote_code_execution], []).
+
+%vuln('remote_code_execution', [web_admin_access], [remote_code_execution], []).
+%vuln('directory-traversal', [web_access], [remote_code_execution], []).
+%vuln('directory-traversal', [web_access], [session_hijack], []).
+%vuln('remote_code_execution', [web_admin_access], [remote_code_execution], []).
