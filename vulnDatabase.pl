f039a1017b53ff3cbcf13e371bd713c37b746316
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2018-05-06 22:54:08 -0400

    Cleanups of the vulnerability database

--- vulnDatabase.pl
+++ vulnDatabase.pl
@@ -6,6 +6,7 @@ vuln('port-scanning', [discover_host], [open_ports], []).
 
 % FTP
 vuln('open-ftp', [open_ports], [ftp_server], [ftp-[]]).
+vuln('directory-traversal', [ftp_server], [user_list], []).
 % Password: secret
 vuln('login-root(brute-force)', [ftp_server], [server_access_root], 
         [ftp-[root_password-(only, ["'$6$GIXVkT.8Wmrfmv9L$evoQa2mqjallkTS9XoxeDiMyrWMt7FqAg4eLR8PgWPcs8Ik/ipiOpcbIgaCnMnxpCTMV6pSBpdzhxe6bapzPc0'"])]]).
@@ -20,28 +21,27 @@ vuln('login-root(credentials)', [passwords, ssh_server], [server_access_root],
         [ssh-[root_password-(only, ['secret'])]]).
 
 % Password: password
-vuln('login-user(brute-force)', [ssh_server], [server_access_user], 
+vuln('login-user(brute-force)', [ssh_server, user_list], [server_access_user], 
+        [ssh-[users-(exists, [example]),
+              passwords-(exists, ["'$6$zXfK0cjCjTOYGk/t$U1DcfcpU2FQnaT5f/RfRHzRpq.va/d.YuDj7HcNW.B87yhaJgPtuik4CWlWa7drm9oGhZ941OXAtSF7CzXIgP1'"])]]).
+vuln('login-user(credentials)', [ssh_server, user_list, passwords], [server_access_user], 
         [ssh-[users-(exists, [example]),
               passwords-(exists, ["'$6$zXfK0cjCjTOYGk/t$U1DcfcpU2FQnaT5f/RfRHzRpq.va/d.YuDj7HcNW.B87yhaJgPtuik4CWlWa7drm9oGhZ941OXAtSF7CzXIgP1'"])]]).
 
-vuln('database_queries', [server_access_user], [user_list, hashed_passwords], 
-        [mysql-[users-(exists, [example]),
-                passwords-(exists, ['user'])]]).
 vuln('crack-hashes', [hashed_passwords], [passwords], []).
 % Password: secret
 vuln('login-root(credentials)', [passwords, ssh_server], [server_access_root], 
         [ssh-[root_password-(only, ["'$6$GIXVkT.8Wmrfmv9L$evoQa2mqjallkTS9XoxeDiMyrWMt7FqAg4eLR8PgWPcs8Ik/ipiOpcbIgaCnMnxpCTMV6pSBpdzhxe6bapzPc0'"])]]).
 
 % Web
-vuln('open-web', [open_ports], [port_80], []).
-vuln('web-access', [port_80], [web_access], 
+vuln('open-web', [open_ports], [web_server], 
         [apache-[]]).
 
-vuln('login-admin(brute-force)', [web_access, login_page], [web_admin_access], []).
+vuln('login-admin(brute-force)', [web_server, login_page], [web_admin_access], []).
 
-vuln('login-admin(credentials)', [web_access, login_page], [web_admin_access], []).
+vuln('login-admin(credentials)', [web_server, login_page, passwords], [web_admin_access], []).
 
-vuln('sql-injection', [web_access], [database_queries], 
+vuln('sql-injection', [login_page], [database_queries], 
         [php-[git_repo-(exists, ['https://KimAChen@bitbucket.org/KimAChen/alpaca_sqli.git']),
               repo_folder-(exists, ['alpaca_sqli'])],
         mysql-[db-(exists, ['alpaca_sqli']),
@@ -49,14 +49,12 @@ vuln('sql-injection', [web_access], [database_queries],
 vuln('db-query-users', [database_queries], [user_list, hashed_passwords], 
         [mysql-[users-(exists, ['alpaca_sqli']), 
                 passwords-(exists, ['password'])]]).
-vuln('crack-hashes', [hashed_passwords], [passwords], []).
-vuln('login-root(credentials)', [passwords, ssh_server], [server_access_root], 
-        [ssh-[root_password-(only, ['secret'])]]).
 
-vuln('directory-traversal', [web_access], [bypass_auth], []).
-vuln('bypass-authentication(admin)', [bypass_auth], [web_admin_access], []).
+vuln('directory-traversal', [web_server], [bypass_auth], []).
+
+%vuln('bypass-authentication(admin)', [bypass_auth], [web_admin_access], []).
 
 %vuln('remote_code_execution', [web_admin_access], [remote_code_execution], []).
-%vuln('directory-traversal', [web_access], [remote_code_execution], []).
-%vuln('directory-traversal', [web_access], [session_hijack], []).
+%vuln('directory-traversal', [web_server], [remote_code_execution], []).
+%vuln('directory-traversal', [web_server], [session_hijack], []).
 %vuln('remote_code_execution', [web_admin_access], [remote_code_execution], []).
