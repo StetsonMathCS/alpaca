f4a0c178296d6c61a459297f27857189c4ac2047
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2018-12-16 23:30:04 -0500

    Symbolic link for ansible role files, and create zip of range files

--- prolog/output.pl
+++ prolog/output.pl
@@ -2,17 +2,19 @@
 :- use_module(library(uuid)).
 :- use_module(library(http/json)).
 
-rangesDir("../ranges").
-
 outputRange(InitialState, Goal, Params, Lattices) :-
     uuid(RangeId, [version(4)]),
     format("Creating range ~s~n", [RangeId]),
-    rangesDir(RangesDir),
-    format(atom(RangeDir), "~s/~s", [RangesDir, RangeId]),
+    format(atom(RangeDirRel), "../ranges/~s", [RangeId]),
+    absolute_file_name(RangeDirRel, RangeDir),
     make_directory_path(RangeDir),
     outputRangeMetadata(RangeDir, RangeId, InitialState, Goal, Params, Lattices),
     printLattices(Lattices),nl,
-    maplist(outputLattice(RangeDir, RangeId), Lattices).
+    maplist(outputLattice(RangeDir, RangeId), Lattices),
+    working_directory(OrigWD, "../ranges"),
+    format(atom(Command), "zip -r ~s.zip ~s", [RangeId, RangeId]),
+    shell(Command),
+    working_directory(_, OrigWD).
 
 outputRangeMetadata(RangeDir, RangeId, InitialState, Goal, Params, Lattices) :-
     format(atom(RangeMetadataFname), "~s/range_metadata.json", [RangeDir]),
@@ -56,28 +58,19 @@ outputLattice(RangeDir, RangeId, Lattice) :-
 
 createYamlFiles(Configs, LatticeDir) :-
     format(atom(AnsibleDir), "~s/ansible", [LatticeDir]),
-    make_directory_path(AnsibleDir),
+    absolute_file_name("../ansible", ParentAnsibleDir),
+    link_file(ParentAnsibleDir, AnsibleDir, symbolic),
+    print(ParentAnsibleDir),nl,
 	formatRoles(Configs, Roles),
-	createPlaybook(Roles, AnsibleDir),
-	format(atom(VarsDir), "~s/ansible/vars", [LatticeDir]),
-	make_directory_path(VarsDir),
-	listRoles(Configs, Vars),
-	createVars(Vars, VarsDir).
+	formatVars(Configs, Vars),
+	createPlaybook(Vars, Roles, LatticeDir).
 
-createPlaybook(Roles, AnsibleDir) :-
-	format(atom(PlaybookFname), "~s/playbook.yml", [AnsibleDir]),
+createPlaybook(Vars, Roles, LatticeDir) :-
+	format(atom(PlaybookFname), "~s/playbook.yml", [LatticeDir]),
     format("Writing ~s~n", [PlaybookFname]),
 	open(PlaybookFname, write, Stream),
 	format(atom(String),
-		"---~n- hosts: all~n~t~2|become: true~n~t~2|vars_files:~n~t~4|- vars/all.yml~n~t~2|roles:~n~s", [Roles]),
-	write(Stream, String),
-	close(Stream).
-
-createVars(Vars, VarsDir) :-
-	format(atom(VarsFname), "~s/all.yml", [VarsDir]),
-    format("Writing ~s~n", [VarsFname]),
-	open(VarsFname, write, Stream),
-	format(atom(String), "---~n~s", [Vars]),
+		"---~n- hosts: all~n~t~2|become: true~n~t~2|vars:~n~s~n~t~2|roles:~n~s", [Vars, Roles]),
 	write(Stream, String),
 	close(Stream).
 
@@ -86,23 +79,24 @@ formatRoles([Role-_|Configs], String) :-
 	formatRoles(Configs, String1),
 	format(atom(String), "~t~4|- ~s~n~s", [Role, String1]).
 
-listRoles([], "").
-listRoles([Role-Val|Rest], String) :-
+formatVars([], "").
+formatVars([Role-Val|Rest], String) :-
 	listKeys(Val, String1),
-	format(atom(Out), "~s:~n~s~n", [Role, String1]),
-	listRoles(Rest, String2),
+	format(atom(Out), "~t~6|~s:~n~s~n", [Role, String1]),
+	formatVars(Rest, String2),
 	format(atom(String), "~s~s", [Out, String2]).
 
 listKeys([], "").
 listKeys([Key-(_, Vals)|Rest], String) :-
 	listVals(Vals, String1),
-	format(atom(Out), "~t~2|~s:~n~s", [Key, String1]),
+	format(atom(Out), "~t~8|~s:~n~s", [Key, String1]),
 	listKeys(Rest, String2),
 	format(atom(String), "~s~s", [Out, String2]).
 
 listVals([], "").
 listVals([Val|Vals], String) :-
 	listVals(Vals, String1),
-	format(atom(String), "~t~4|- ~s~n~s", [Val, String1]).
+	format(atom(String), "~t~10|- ~s~n~s", [Val, String1]).
 listVals(Val, String) :-
-	format(atom(String), "~t~4|- ~s~n", [Val]).
+	format(atom(String), "~t~10|- ~s~n", [Val]).
+
